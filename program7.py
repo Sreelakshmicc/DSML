85c996866302da1bb6b52c80205bf76906e48784
Sreelakshmicc <sreelakshmicth@gmail.com>
2025-07-07 21:28:40 +1200

    Update program7.py

--- program7.py
+++ program7.py
@@ -3,11 +3,8 @@ a=np.array([[1,2,3,4],[5,6,7,8],[4,3,2,1]])
 print("Array elements are:",a)
 print("Sum of all elements:")
 print(np.sum(a))
-
-
 print("Sum of each column:")
 print(np.sum(a, axis=0))
-
 print("Sum of each row:")
 print(np.sum(a, axis=1)) 
 
