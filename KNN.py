b5e1a79f66722db3995dff10333d9ddce0f0b8df
Sreelakshmicc <sreelakshmicth@gmail.com>
2025-09-09 09:13:45 +0000

    Update KNN.py

--- KNN.py
+++ KNN.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
 from sklearn import metrics
 from sklearn.neighbors import KNeighborsClassifier
 
